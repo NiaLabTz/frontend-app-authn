d975542078aec4d34a9a7dc36fd5894d70ef388e
Calvin Jackson <amigohmccclurkin98@gmail.com>
2023-12-06 15:05:10 +0300

    wip

--- src/register/RegistrationPage.jsx
+++ src/register/RegistrationPage.jsx
@@ -71,7 +71,7 @@ class RegistrationPage extends React.Component {
     this.tpaHint = getTpaHint();
     const { registrationFormData } = this.props;
     this.state = {
-      country: 'Tanzania',
+      country: '',
       email: registrationFormData.email,
       name: registrationFormData.name,
       password: registrationFormData.password,
